ad1457628f6d62baf1922d1742e3e7c28bb747a3
param-ninthu <67898034+param-ninthu@users.noreply.github.com>
2021-02-04 19:22:23 +0530

    Delete.js

---
+++ includes/delete.js
@@ -0,0 +1,9 @@
+function delete_user()
+    {
+        confirm('Are You sure and you want to delete this User ?');
+    }
+
+    function delete_account()
+    {
+        confirm('Are You sure and you want to delete the account ?');
+    }
